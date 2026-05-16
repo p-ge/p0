d58806fe07b23713eb202aec2e9615c00358d259
p-ge <brandonyuan12@gmail.com>
2026-05-16 20:58:43 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,8 +66,8 @@ BLOCKED_WORDS = [
     "nigga", "nigger", "niga",
     "skid", "skidded",
     "skidder", "skidding", "script kiddie", "scriptkiddie", "sk1d", "sk!d", "sk!dded",
-    "skidd", "skido"
-    ".gg", "gg/"
+    "skidd", "skido", 
+    ".gg/"
 ]
 
 WHITELIST_WORDS = [
